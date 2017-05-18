941f0216d7fdb9e05766652efd3141da1887a689
Jordan Benner <jordanbenner@outlook.com>
2017-05-18 11:41:53 -0500

    Problem 4

--- excersise.js
+++ excersise.js
@@ -15,3 +15,18 @@ function madlib(name, subject){
   let output = name + 'favorite subject in school is' + subject;
   return output;
 }
+
+// Tip Calculator//
+var bill = 100
+function tipAmount(bill, service) {
+
+  if (service == 'good') {
+    console.log(bill * .20);
+  } else if (service == 'fair') {
+    console.log(bill * .15);
+  } else if (service == 'bad'){
+    console.log(bill * .10);
+  }
+
+}
+tipAmount(100, 'good')

1af2fc18e12f8f4a5618c297cdfa199e673fc10a
Jordan Benner <jordanbenner@outlook.com>
2017-05-18 11:59:46 -0500

    Problem 5

--- excersise.js
+++ excersise.js
@@ -30,3 +30,31 @@ function tipAmount(bill, service) {
 
 }
 tipAmount(100, 'good')
+
+// Tip Calculator 2 //
+var bill = 100
+
+function totalAmount(bill, service, total){
+  if (service == 'good') {
+    console.log(bill + bill * .20 );
+  } else if (service == 'fair') {
+    console.log(bill + bill * .15);
+  } else if (service == 'bad'){
+    console.log(bill + bill * .10);
+  }
+}
+totalAmount(100, 'good')
+
+//Tip Calculator 3 //
+var bill = 100
+
+function splitAmount(bill, service, total){
+  if (service == 'good') {
+    console.log(bill + bill * .20 );
+  } else if (service == 'fair') {
+    console.log(bill + bill * .15);
+  } else if (service == 'bad'){
+    console.log(bill + bill * .10);
+  }
+}
+splitAmount(100, 'good')

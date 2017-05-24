93bf9b24b7ef86629b3b164d965ddaf5045ea117
Jordan Benner <jordanbenner@outlook.com>
2017-05-24 11:58:57 -0500

    objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+//object exercise 1//
+function Person(name, email, phone) {
+  this.name = name;
+  this.email = email;
+  this.phone = phone;
+}
+
+Person.prototype.greet = function(other) {
+  console.log(`Hello ${other.name}, I am ${this.name} my email is ${this.email} and my direct line is ${this.phone}.`);
+};
+
+var Jordan = new Person('Jordan', 'jordan@aol.com', '495-586-3456');
+var Sonny = new Person('Sonny', 'sonny@hotmail.com', '483-586-4948');
+
+Jordan.greet(Sonny);

2c85026d5700a6c80379e1ff14226ebef4011625
Jordan Benner <jordanbenner@outlook.com>
2017-05-24 12:13:16 -0500

    Info statement

--- objects.js
+++ objects.js
@@ -8,8 +8,13 @@ function Person(name, email, phone) {
 Person.prototype.greet = function(other) {
   console.log(`Hello ${other.name}, I am ${this.name} my email is ${this.email} and my direct line is ${this.phone}.`);
 };
+Person.prototype.info =function(){
+  console.log(`My Contact information is ${this.email} ${this.phone}`);
+};
+
 
 var Jordan = new Person('Jordan', 'jordan@aol.com', '495-586-3456');
 var Sonny = new Person('Sonny', 'sonny@hotmail.com', '483-586-4948');
 
 Jordan.greet(Sonny);
+Jordan.info();

f11c2a66b303deeb583e927d7003445a6649da04
Jordan Benner <jordanbenner@outlook.com>
2017-05-25 13:02:02 -0500

    Objects class from es5 to es6

--- objects2.js
+++ objects2.js
@@ -30,9 +30,27 @@ var daughter = {
   hairColor: 'brown',
   showinfo: function(){
     console.log(daughter.firstName + ' ' + daughter.hairColor);
-  }
 };
 
 daughter.__proto__ = mom;
 mom.showInfo();
 console.log(daughter.firstName + ' ' + mom.lastName + ' ' + mom.eyeColor + ' ' + daughter.hairColor);
+
+//Person //
+
+class Person {
+  constructor(name){
+    this.name = name;
+    this.friends = [];
+  }
+  greet(other) {
+    console.log('Yo ' + other.name + '! from ' + this.name + '.')
+  }
+}
+class Friend extends Person {
+  friend(){
+    console.log(this.friends.push(friend));
+  }
+}
+var f = new Friend('Jordan');
+f.friend();

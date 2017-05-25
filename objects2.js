0f80b7bc54c9cc9a07c0b299088ce3d93cdcd9ac
Jordan Benner <jordanbenner@outlook.com>
2017-05-25 11:37:07 -0500

    Part two inheritance

--- objects2.js
+++ objects2.js
@@ -1,4 +1,4 @@
-//inheritance//
+//inheritance one//
 
 var mom = {
   firstName: 'Alice',
@@ -13,4 +13,26 @@ var daughter = {
 };
 
 daughter.__proto__ = mom;
+console.log(daughter.firstName + ' ' + mom.lastName + ' ' + mom.eyeColor + ' ' + daughter.hairColor);
+
+//inheritance two //
+var mom = {
+  firstName: 'Alice',
+  lastName: 'Wong',
+  eyeColor: 'brown',
+  hairColor: 'black',
+  showInfo: function(){
+    console.log(mom.firstName + ' ' + mom.lastName + ' ' + mom.eyeColor + ' ' + mom.hairColor);
+  }
+};
+var daughter = {
+  firstName: 'Ilene',
+  hairColor: 'brown',
+  showinfo: function(){
+    console.log(daughter.firstName + ' ' + daughter.hairColor);
+  }
+};
+
+daughter.__proto__ = mom;
+mom.showInfo();
 console.log(daughter.firstName + ' ' + mom.lastName + ' ' + mom.eyeColor + ' ' + daughter.hairColor);
